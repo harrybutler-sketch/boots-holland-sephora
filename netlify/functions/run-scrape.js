2b8c6c9cbc0718785925700eae83c0e830c1887a
Harry.Butler <harry.butler@macbookair.home>
2026-04-20 14:36:13 +0100

    Update Morrisons scraper link to frozen food category

--- netlify/functions/run-scrape.js
+++ netlify/functions/run-scrape.js
@@ -122,8 +122,7 @@ export const handler = async (event, context) => {
       }
       if (pRetailers.some(r => r.includes('morrisons'))) {
         const morrisonsUrls = [
-          'https://groceries.morrisons.com/categories/fresh-chilled-foods/176739?boolean=new&sortBy=favorite',
-          'https://groceries.morrisons.com/categories/food-cupboard/102705?boolean=new&sortBy=favorite'
+          'https://groceries.morrisons.com/categories/frozen-food/180331?boolean=new&sortBy=favorite'
         ];
         morrisonsUrls.forEach(url => startUrls.push({ url, userData: { retailer: 'Morrisons', label: 'LISTING' } }));
       }
